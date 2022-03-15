9be564c365c7b48129e3e7246451fbdc73434cb9
paayima <paayima94@gmailcom>
2022-03-15 18:27:21 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "alxschool"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "alxschool";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
+}
+	
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My function nested loops readme
