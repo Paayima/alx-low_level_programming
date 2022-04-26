0815ef4679f78c05b1554d06390abdc16db5597f
paayima <paayima94@gmailcom>
2022-04-26 15:21:19 +0000

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,15 +16,19 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
+
 	temp = *head;
+
 	while (temp->next != NULL)
 		temp = temp->next;
 
 	temp->next = new;
+
 	return (new);
 }
